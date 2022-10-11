9353f6c205e55adf32015a9a5c49beab2425ff37
abhixnv <mabhinav534@gmail.com>
2022-10-12 00:00:25 +0530

    initial stage

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class PongGame(Widget):
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
 
-    def serve_ball(self, vel=(10, 0)):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
@@ -45,10 +45,10 @@ class PongGame(Widget):
 
         if self.ball.x < self.x:
             self.player2.score += 1
-            self.serve_ball(vel=(10, 0))
+            self.serve_ball(vel=(4, 0))
         if self.ball.right > self.width:
             self.player1.score += 1
-            self.serve_ball(vel=(-10, 0))
+            self.serve_ball(vel=(-4, 0))
 
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
